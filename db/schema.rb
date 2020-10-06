f379099603e08d5ebd148173957dfc07f06cbe67
Sasaki Yudai <guangdazuomu0@gmail.com>
2020-10-06 14:32:15 +0000

    kadai

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_013955) do
+ActiveRecord::Schema.define(version: 2020_10_06_141137) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "stasus"
+    t.string "status"
   end
 
 end
